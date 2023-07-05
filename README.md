84bfc2dd9506c3944c574726963e37b1f720abb1
Paul Lopez <paul.pilm@gmail.com>
2023-07-04 17:37:29 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+### React Custom Hook 
+
+Custom hook to make http requests to Firebase, and store a Todo list.
+![Index](Assets/todolist.png)
+
+#### Installation
+
+1. Install dependencies
+
+```
+yarn install
+```
+
+2. Create you database on Firebase and get the url
+
+[Firebase Console](https://console.firebase.google.com/)
+
+3. Create your env file from sample one
+
+```
+cp .env.sample .env
+```
+
+4. Run your local app
+
+```
+yarn run start
+```
+
+5. Open your browser and go to 
+```
+http://localhost:3000/
+```
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useState, Fragment } from 'react';
+import useHttp from './hooks/use-http';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (taskObj) => {
+      const loadedTasks = [];
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    }
+    fetchTasks({ url: process.env.REACT_APP_FIREBASE_URL }, transformTasks);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </Fragment>
+  );
+}
+
+export default App;
